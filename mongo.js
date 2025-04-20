82122c9714444720d042d0a100df9036f412a7f2
devipreya <160410910+devipreya@users.noreply.github.com>
2025-04-20 18:51:15 +0530

    Update mongo.js

--- mongo.js
+++ mongo.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost:27017/loginSignupPage")
+mongoose.connect("process.env.MONGO_URI")
     .then(() => {
         console.log("MongoDB connected successfully");
     })
